1bbff33e270c92b9e877b60a9c3a447c133a4cf7
Jack Hunter <jhunter5@binghamton.edu>
2025-08-14 16:50:18 -0400

    updated crash fix with new index.html

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Test from './Test.jsx'
 import LandingPage from './pages/LandingPage.jsx'
 
 createRoot(document.getElementById('root')).render(
